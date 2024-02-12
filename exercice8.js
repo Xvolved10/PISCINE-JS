b6398a12cc15a4937271e60cbcfb6d34c28b9ef2
Candidat-005 <maxime120496@hotmail.fr>
2024-02-12 13:56:26 +0100

    correction exercice8

--- exercice8.js
+++ exercice8.js
@@ -1,6 +1,5 @@
 // Exercice 8: Recherche dans un tableau
 var table = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var result = table.find((element) => element === 6);
 
-for (let i = 0; i < table.length; i++) {
-    console.log(table[i]);
-}
+console.log("Le chiffre " + result + " est dans le tableau");
